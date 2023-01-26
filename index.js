10622945ae3d58609d2cc89d1caab60a11bbdda1
Andrew Kovalenko <goldenduke14@gmail.com>
2023-01-26 16:50:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,3 +36,17 @@ console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
 // Значення змінної hexColors - це масив ["#f44336", "#2196f3", "#4caf50", "#ffeb3b"]
 // Оголошена змінна rgbColors
 // Значення змінної rgbColors - це масив ["244,67,54", "33,150,243", "76,175,80", "255,235,59"]
+
+const colors = [
+  { hex: "#f44336", rgb: "244,67,54" },
+  { hex: "#2196f3", rgb: "33,150,243" },
+  { hex: "#4caf50", rgb: "76,175,80" },
+  { hex: "#ffeb3b", rgb: "255,235,59" },
+];
+
+const hexColors = [];
+const rgbColors = [];
+// Change code below this line
+
+for (const color of colors) {
+}
