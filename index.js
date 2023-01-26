06338a63fe0eae1f9bd6eb68779fe09ba6115571
Andrew Kovalenko <goldenduke14@gmail.com>
2023-01-26 16:40:59 +0200

    create repo

---
+++ index.js
@@ -0,0 +1,38 @@
+
+//                            task 16
+// Напиши функцію countTotalSalary(salaries), яка приймає об'єкт зарплат, де ім'я властивості - це ім'я співробітника, а значення властивості - це зарплата. Функція повинна розрахувати загальну суму зарплат співробітників і повернути її. Використовуй змінну totalSalary для зберігання загальної суми зарплати.
+
+// Оголошена функція countTotalSalary(salaries)
+// Виклик countTotalSalary({}) повертає 0
+// Виклик countTotalSalary({ mango: 100, poly: 150, alfred: 80 }) повертає 330
+// Виклик countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }) повертає 400
+// Функція враховує тільки власні властивості об'єкта
+
+
+function countTotalSalary(salaries) {
+  let totalSalary = 0;
+  // Change code below this line
+
+  const keys = Object.keys(salaries);
+  const values = [];
+  for (const key of keys) {
+    values.push(salaries[key]);
+    totalSalary += salaries[key];
+  }
+
+  // Change code above this line
+  return totalSalary;
+}
+
+console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+
+
+//                             task 17
+// Перебери масив об'єктів colors, використовуючи цикл for...of. Додай у масив hexColors значення властивостей hex, а в масив rgbColors - значення властивостей rgb з усіх об'єктів масиву colors.
+
+// Оголошена змінна colors
+// Значення змінної colors - це масив
+// Оголошена змінна hexColors
+// Значення змінної hexColors - це масив ["#f44336", "#2196f3", "#4caf50", "#ffeb3b"]
+// Оголошена змінна rgbColors
+// Значення змінної rgbColors - це масив ["244,67,54", "33,150,243", "76,175,80", "255,235,59"]
