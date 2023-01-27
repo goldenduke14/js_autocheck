2a3ebb59342fd965584a5379106b200055965155
Andrew Kovalenko <goldenduke14@gmail.com>
2023-01-27 16:11:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,22 +9,22 @@
 // Функція враховує тільки власні властивості об'єкта
 
 
-function countTotalSalary(salaries) {
-  let totalSalary = 0;
-  // Change code below this line
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   // Change code below this line
 
-  const keys = Object.keys(salaries);
-  const values = [];
-  for (const key of keys) {
-    values.push(salaries[key]);
-    totalSalary += salaries[key];
-  }
+//   const keys = Object.keys(salaries);
+//   const values = [];
+//   for (const key of keys) {
+//     values.push(salaries[key]);
+//     totalSalary += salaries[key];
+//   }
 
-  // Change code above this line
-  return totalSalary;
-}
+//   // Change code above this line
+//   return totalSalary;
+// }
 
-console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
 
 
 //                             task 17
@@ -37,21 +37,21 @@ console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
 // Оголошена змінна rgbColors
 // Значення змінної rgbColors - це масив ["244,67,54", "33,150,243", "76,175,80", "255,235,59"]
 
-const colors = [
-  { hex: "#f44336", rgb: "244,67,54" },
-  { hex: "#2196f3", rgb: "33,150,243" },
-  { hex: "#4caf50", rgb: "76,175,80" },
-  { hex: "#ffeb3b", rgb: "255,235,59" },
-];
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
 
-const hexColors = [];
-const rgbColors = [];
-// Change code below this line
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
 
-for (const color of colors) {
-  hexColors.push(color.hex);
-  rgbColors.push(color.rgb);
-};
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// };
 
 //                    task 18
 // Напиши функцію getProductPrice(productName), яка приймає один параметр productName - назва продукту. Функція шукає об'єкт продукту з таким ім'ям (властивість name) в масиві products і повертає його ціну (властивість price). Якщо продукт з такою назвою не знайдений, функція повинна повертати null.
@@ -63,29 +63,29 @@ for (const color of colors) {
 // Виклик getProductPrice("Droid") повертає 400.
 // Виклик getProductPrice("Engine") повертає null.
 
-const products = [
-  { name: "Radar", price: 1300, quantity: 4 },
-  { name: "Scanner", price: 2700, quantity: 3 },
-  { name: "Droid", price: 400, quantity: 7 },
-  { name: "Grip", price: 1200, quantity: 9 },
-];
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
 
-function getProductPrice(productName) {
-  // Change code below this line
-  for (const product of products) {
-    if (productName === product.name) {
-      return product.price;
-    }
-    // Change code above this line
-  }
-  return null;
-}
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   for (const product of products) {
+//     if (productName === product.name) {
+//       return product.price;
+//     }
+//     // Change code above this line
+//   }
+//   return null;
+// }
 
-console.log(getProductPrice("Radar"));
-console.log(getProductPrice("Grip"));
-console.log(getProductPrice("Scanner"));
-console.log(getProductPrice("Droid"));
-console.log(getProductPrice("Engine"));
+// console.log(getProductPrice("Radar"));
+// console.log(getProductPrice("Grip"));
+// console.log(getProductPrice("Scanner"));
+// console.log(getProductPrice("Droid"));
+// console.log(getProductPrice("Engine"));
 
 
 //                     task 19
@@ -98,14 +98,64 @@ console.log(getProductPrice("Engine"));
 // Виклик getAllPropValues("category") повертає []
 
 
-const products = [
-  { name: "Radar", price: 1300, quantity: 4 },
-  { name: "Scanner", price: 2700, quantity: 3 },
-  { name: "Droid", price: 400, quantity: 7 },
-  { name: "Grip", price: 1200, quantity: 9 },
-];
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
 
-function getAllPropValues(propName) {
-  // Change code below this line
-  // Change code above this line
-}
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//   const propValue = [];
+
+//   for (const product of products) {
+//     if (product[propName]) {
+//       propValue.push(product[propName]);
+//     }
+//   }
+//   return propValue;
+//   // Change code above this line
+// }
+
+// console.log(getAllPropValues("name"));
+// console.log(getAllPropValues("quantity"));
+// console.log(getAllPropValues("price"));
+
+
+//                         task 20
+// Напиши функцію calculateTotalPrice(productName), яка приймає один параметр productName - назва товару. Функція повинна повернути загальну вартість (ціна * кількість) товару з таким ім'ям з масиву products.
+
+// Оголошена функція calculateTotalPrice(productName)
+// Виклик calculateTotalPrice("Blaster") повертає 0
+// Виклик calculateTotalPrice("Radar") повертає 5200
+// Виклик calculateTotalPrice("Droid") повертає 2800
+// Виклик calculateTotalPrice("Grip") повертає 10800
+// Виклик calculateTotalPrice("Scanner") повертає 8100
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   // Change code below this line
+//   let totalPrice = 0;
+
+//   for (const product of products) {
+//     if (productName === product.name) {
+//       totalPrice = product.price * product.quantity;
+//     }
+//   }
+
+//   return totalPrice;
+//   // Change code above this line
+// }
+
+// console.log(calculateTotalPrice("Blaster"));
+// console.log(calculateTotalPrice("Radar"));
+// console.log(calculateTotalPrice("Droid"));
+// console.log(calculateTotalPrice("Grip"));
+// console.log(calculateTotalPrice("Scanner"));
