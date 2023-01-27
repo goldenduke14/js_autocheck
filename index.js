b853eff10054af7df59ac3fe959c0ea8488cbe59
Andrew Kovalenko <goldenduke14@gmail.com>
2023-01-27 14:04:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -72,5 +72,40 @@ const products = [
 
 function getProductPrice(productName) {
   // Change code below this line
+  for (const product of products) {
+    if (productName === product.name) {
+      return product.price;
+    }
+    // Change code above this line
+  }
+  return null;
+}
+
+console.log(getProductPrice("Radar"));
+console.log(getProductPrice("Grip"));
+console.log(getProductPrice("Scanner"));
+console.log(getProductPrice("Droid"));
+console.log(getProductPrice("Engine"));
+
+
+//                     task 19
+// Напиши функцію getAllPropValues(propName), яка приймає один параметр propName - ім'я (ключ) властивості. Функція повинна повернути масив всіх значень властивості з таким ім'ям з кожного об'єкта в масиві products. Якщо в об'єктах відсутні властивості з таким ім'ям, функція повинна повернути порожній масив.
+
+// Оголошена функція getAllPropValues(propName)
+// Виклик getAllPropValues("name") повертає ["Radar", "Scanner", "Droid", "Grip"]
+// Виклик getAllPropValues("quantity") повертає [4, 3, 7, 9]
+// Виклик getAllPropValues("price") повертає [1300, 2700, 400, 1200]
+// Виклик getAllPropValues("category") повертає []
+
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getAllPropValues(propName) {
+  // Change code below this line
   // Change code above this line
 }
